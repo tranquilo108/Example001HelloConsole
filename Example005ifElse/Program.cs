88158d49d748a023dcc72b02c7c6290f809e4292
tranquilo108 <sanbonzakurakogyoshi@gmail.com>
2022-11-27 20:16:33 +0700

    add Example 007 Cicle Треугольник Паскаля

---
+++ Example005ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Привет, как тебя зовут?: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!!! ");
+}
+else
+{
+    Console.Write("Добро пожаловать, ");
+    Console.Write(username);
+}
