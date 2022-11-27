8c36f4a3a8d014c56f0862dd394f67c65e46d65e
tranquilo108 <sanbonzakurakogyoshi@gmail.com>
2022-11-27 18:53:40 +0700

    add random

---
+++ Example003Calc/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 100); // 1 2 3 4 .... 99
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100); // 1 2 3 4 .... 99
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
---
+++ Example004Div/Program.cs
@@ -0,0 +1,4 @@
+﻿double numberA = 17;
+double numberB = 5;
+double result = numberA / numberB;
+Console.WriteLine(result);
